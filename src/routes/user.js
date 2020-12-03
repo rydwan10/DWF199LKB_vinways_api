ebf74162f1c2803ec6ad8bb1b3f3a3d5c03a46d6
Muhammad Rydwan <muhammadrydwanmylike10@gmail.com>
2020-12-03 20:04:55 +0700

    Initial commit for master

--- src/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getUsers } = require('../controllers/user');
-
-router.get('/users', getUsers);
-
-module.exports = router;
