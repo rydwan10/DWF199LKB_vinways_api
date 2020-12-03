c6178ebe43c8f1f979db4c81701c811b232c75fc
Muhammad Rydwan <muhammadrydwanmylike10@gmail.com>
2020-12-03 21:45:20 +0700

    Add: Adding Create, Update, and Delete functionality

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -59,9 +59,140 @@ exports.getArtistById = async (req, res) => {
 
     res.status(200).send({
       status: "success",
-      message: "Data successfully retrieved!",
+      message: "Artist successfully retrieved!",
       data: {
         artist,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      error: {
+        message: "Server Error",
+      },
+    });
+  }
+};
+
+//! Attention below are the controllers that must have authorization header later!
+exports.addArtist = async (req, res) => {
+  const data = req.body;
+  try {
+    // jika objek data tidak memiliki key dan jika objek data kurang dari 5 fields
+    if (
+      data === undefined ||
+      Object.keys(data).length === 0 ||
+      Object.keys(data).length < 5
+    ) {
+      return res.status(400).send({
+        status: "Bad Request",
+        message: `You must fill all the fields! (name, old, category, startCareer, thumbnail)`,
+        data: {
+          artist: null,
+        },
+      });
+    }
+
+    const newArtist = await Artist.create(data);
+
+    res.status(201).send({
+      status: "success",
+      message: "New artist is successfully added!",
+      data: {
+        newArtist,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      error: {
+        message: "Server Error",
+      },
+    });
+  }
+};
+
+exports.deleteArtistById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const artist = await Artist.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!artist) {
+      return res.status(404).send({
+        status: "Not Found",
+        message: `Artist with id: ${id} is not found!`,
+        data: {
+          artist: null,
+        },
+      });
+    }
+
+    await Artist.destroy({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).send({
+      status: "success",
+      message: "Artist successfully deleted!",
+      data: {
+        deletedArtist: artist,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({
+      error: {
+        message: "Server Error",
+      },
+    });
+  }
+};
+
+exports.updateArtistById = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  try {
+    const artist = await Artist.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!artist) {
+      return res.status(404).send({
+        status: "Not Found",
+        message: `Artist with id: ${id} is not found!`,
+        data: {
+          artist: null,
+        },
+      });
+    }
+
+    await Artist.update(data, {
+      where: {
+        id,
+      },
+    });
+
+    const updatedArtist = await Artist.findOne({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).send({
+      status: "success",
+      message: "Artist successfully updated!",
+      data: {
+        updatedArtistData: updatedArtist,
+        oldArtistData: artist,
       },
     });
   } catch (err) {
